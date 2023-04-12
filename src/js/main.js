ea23eb9ed3a0a13d121d393bde032f3977bf7d7c
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-04-12 22:17:02 +0300

    link about.css

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,6 @@ import "../css/reset.css";
 import "../css/utility.css";
 import "../css/index.css";
 import "../css/home.css";
+import "../css/about.css";
 import "../css/footer.css";
 // import "./style.css";
